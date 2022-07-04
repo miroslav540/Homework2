0d87f246156030566c932b93e96ba7061c0cbd80
miroslav540 <107503510+miroslav540@users.noreply.github.com>
2022-07-04 21:45:27 +0300

    очередная ошибочка)

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,7 +1,7 @@
 ﻿ Console.WriteLine("enter your number");
  int num = Convert.ToInt32(Console.ReadLine());
  int[] a = new int[num];
- for (int i = 1;i < num;  i++)
+ for (int i = 1;i <= num;  i++)
  {
     a[i - 1] = i * i * i;
     Console.WriteLine(a[i - 1]);
